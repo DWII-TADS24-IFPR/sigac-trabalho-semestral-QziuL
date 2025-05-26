c90b294e8dcbe9af8ebb484e8cb536fd2000a466
QziuL <lf.quinholi2@gmail.com>
2025-05-26 09:23:52 -0300

    build: initial commit

---
+++ sigac/resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    @vite(['resources/js/app.js'])
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>SIGAC</title>
+</head>
+<body>
+    <div class="card" style="width: 20rem; text-align: center; margin: auto">
+        <div class="card-body">
+            <h1 class="card-title">Bem vindo ao SIGAC</h1>
+        </div>
+    </div>
+</body>
+</html>
