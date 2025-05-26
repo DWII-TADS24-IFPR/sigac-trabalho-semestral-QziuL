210336b96cd794855a3a4db87183e1be3f6619c2
QziuL <lf.quinholi2@gmail.com>
2025-05-26 11:24:53 -0300

    style: layout welcome view

--- sigac/resources/views/welcome.blade.php
+++ sigac/resources/views/welcome.blade.php
@@ -1,15 +1,15 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    @vite(['resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>SIGAC</title>
 </head>
 <body>
-    <div class="card" style="width: 20rem; text-align: center; margin: auto">
-        <div class="card-body">
-            <h1 class="card-title">Bem vindo ao SIGAC</h1>
+    <div class="w-20 h-20 fill-current " style="width: 20rem; text-align: center; margin: auto">
+        <div class="mt-4">
+            <h1 class="block font-bold text-black dark:text-gray-300">Bem vindo ao SIGAC</h1>
         </div>
     </div>
 </body>
