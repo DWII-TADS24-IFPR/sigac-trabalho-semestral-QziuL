0ba7760b47a23e62baa1537b3454e7c5f091baa8
QziuL <lf.quinholi2@gmail.com>
2025-05-27 20:26:48 -0300

    feat: add method for verify if user is admin or aluno

--- sigac/app/Models/User.php
+++ sigac/app/Models/User.php
@@ -22,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'is_admin'
     ];
 
     /**
@@ -45,5 +46,15 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function isAdmin(): bool
+    {
+        return !is_null($this->isAdmin());
+    }
+
+    public function isAluno(): bool
+    {
+        return is_null($this->isAdmin());
     }
 }
