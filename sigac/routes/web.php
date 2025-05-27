3f265d5ec64c88b79d02e59c4c6e7063b4fe4589
QziuL <lf.quinholi2@gmail.com>
2025-05-27 20:24:00 -0300

    feat: add routes

--- sigac/routes/web.php
+++ sigac/routes/web.php
@@ -1,20 +1,37 @@
 <?php
 
+use App\Http\Controllers\AlunoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ComprovanteController;
+use App\Http\Controllers\CursoController;
+use App\Http\Controllers\DeclaracaoController;
+use App\Http\Controllers\DocumentoController;
+use App\Http\Controllers\NivelController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TurmaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home')->middleware(['auth']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
+//
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('aluno', AlunoController::class);
+Route::resource('categoria', CategoriaController::class);
+Route::resource('comprovante', ComprovanteController::class);
+Route::resource('curso', CursoController::class);
+Route::resource('declaracao', DeclaracaoController::class);
+Route::resource('documento', DocumentoController::class);
+Route::resource('nivel', NivelController::class);
+Route::resource('turma', TurmaController::class);
 
 require __DIR__.'/auth.php';
