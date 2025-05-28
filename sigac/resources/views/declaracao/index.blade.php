e100d594d1749738012975cf350d5b6732b92965
QziuL <lf.quinholi2@gmail.com>
2025-05-27 23:38:34 -0300

    feat: views for declaracao

---
+++ sigac/resources/views/declaracao/index.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.main')
+
+@section('title', 'Declarações')
+
+@section('content')
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+
+    <div class="container-fluid px-4 row">
+        <div class="col-xl vh-100">
+            <div class="card p-4 border-light mb-3">
+                <h4 class="text-center bg-black text-white rounded-top-5 p-2">Declarações</h4>
+                <a class="text-center fs-5 link-underline-info" href="{{ route('declaracao.create' )}}">
+                    Gerar declaracao
+                </a>
+                @if($declaracoes->isEmpty())
+                    <p class="text-center fs-5 mt-3">Não há declarações...</p>
+                @else
+                    <table class="table table-striped">
+
+                        <thead>
+                        <tr>
+                            <th scope="col">Data</th>
+                            <th scope="col">Aluno</th>
+                            <th scope="col">Comprovante ID</th>
+                            <th scope="col" class="text-center">Ação</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+
+                        @foreach($declaracoes as $declaracao)
+                            <tr>
+                                <td>{{$declaracao->data}}</td>
+                                <td>{{$declaracao->aluno->nome}}</td>
+                                <td>{{$declaracao->comprovante->id}}</td>
+                                <td class="d-flex justify-content-around">
+                                    <form method="get"
+                                          class="d-inline m-0 p-0"
+                                          action="{{ route('declaracao.edit', $declaracao->id) }}" >
+                                        @csrf
+
+                                        <button class="btn m-0 p-0" type="submit">
+                                            <i class="fa fa-pencil" aria-hidden="true"></i>
+                                        </button>
+                                    </form>
+
+                                    <form method="post" class="d-inline m-0 p-0"
+                                          action="{{ route('declaracao.destroy', $declaracao->id) }}"
+                                          onsubmit="return confirm('Tem certeza que deseja excluir?');" >
+                                        @csrf
+
+                                        @method('delete')
+                                        <button class="btn m-0 p-0" type="submit">
+                                            <i class="fa fa-trash" aria-hidden="true"></i>
+                                        </button>
+                                    </form>
+                                </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                @endif
+            </div>
+        </div>
+    </div>
+@endsection
