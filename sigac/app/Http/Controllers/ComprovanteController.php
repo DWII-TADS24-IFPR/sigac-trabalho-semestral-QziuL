f2086c8b95c94af1d854539b8940e8be00583690
QziuL <lf.quinholi2@gmail.com>
2025-05-27 23:40:15 -0300

    refactor: changed routes for return

--- sigac/app/Http/Controllers/ComprovanteController.php
+++ sigac/app/Http/Controllers/ComprovanteController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Comprovante;
 use App\Repositories\AlunoRepository;
+use App\Repositories\CategoriaRepository;
 use App\Repositories\ComprovanteRepository;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -13,6 +14,8 @@ class ComprovanteController extends Controller
 {
     private ComprovanteRepository $repository;
     private AlunoRepository $alunoRepository;
+
+    private CategoriaRepository $categoriaRepository;
 
     private array $regrasValidacao = [
         'horas' => 'required|integer|min:1|max:999',
@@ -35,6 +38,7 @@ class ComprovanteController extends Controller
     {
         $this->repository = new ComprovanteRepository();
         $this->alunoRepository = new AlunoRepository();
+        $this->categoriaRepository = new CategoriaRepository();
     }
 
 
@@ -47,10 +51,11 @@ class ComprovanteController extends Controller
     public function create(): View
     {
         $alunos = $this->alunoRepository->selectAll();
-        return view('comprovante.create', compact('alunos'));
+        $categorias = $this->categoriaRepository->selectAll();
+        return view('comprovante.create', compact('alunos', 'categorias'));
     }
 
-    public function store(Request $request): View
+    public function store(Request $request): RedirectResponse
     {
         $request->validate($this->regrasValidacao, $this->mensagemErro);
 
@@ -62,28 +67,28 @@ class ComprovanteController extends Controller
         $comprovante->setUserId($request->input('user_id'));
         $comprovante->save();
 
-        return view('comprovante.create')->with('sucess', 'Comprovante cadastrado com sucesso.');
+        return redirect()->route('comprovante.index')->with('success', 'Comprovante cadastrado com sucesso.');
     }
 
-    public function show(int $id): View
+    public function show(int $id): View | RedirectResponse
     {
         $comprovante = $this->find($id);
-
         return (isset($comprovante))
             ? view('comprovante.show', compact('comprovante'))
-            : view('comprovante.index')->with('error','Comprovante não encontrado.');
+            : redirect()->route('comprovante.index')->with('error','Comprovante não encontrado.');
     }
 
-    public function edit(int $id): View
+    public function edit(int $id): View | RedirectResponse
     {
         $comprovante = $this->find($id);
-
+        $categorias = $this->categoriaRepository->selectAll();
+        $alunos = $this->alunoRepository->selectAll();
         return (isset($comprovante))
-            ? view('comprovante.edit', compact('comprovante'))
-            : view('comprovante.index')->with('error', 'Comprovante não encontrado.');
+            ? view('comprovante.edit', compact('comprovante', 'categorias', 'alunos'))
+            : redirect()->route('comprovante.index')->with('error', 'Comprovante não encontrado.');
     }
 
-    public function update(Request $request, int $id): View
+    public function update(Request $request, int $id): RedirectResponse
     {
         $request->validate($this->regrasValidacao, $this->mensagemErro);
 
@@ -93,22 +98,22 @@ class ComprovanteController extends Controller
         {
             $comprovante->setHoras($request->input('horas'));
             $comprovante->setAtividade(mb_strtoupper($request->input('atividade')));
-            $comprovante->setCategoriaId($request->input('categoria_id'));
-            $comprovante->setAlunoId($request->input('aluno_id'));
-            $comprovante->setUserId($request->input('user_id'));
+            $comprovante->setCategoriaId(intval($request->input('categoria_id')));
+            $comprovante->setAlunoId(intval($request->input('aluno_id')));
+            $comprovante->setUserId(intval($request->input('user_id')));
             $comprovante->save();
 
-            return view('comprovante.index')->with('sucess', 'Comprovante atualizado com sucesso.');
+            return redirect()->route('comprovante.index')->with('success', 'Comprovante atualizado com sucesso.');
         }
 
-        return view('comprovante.index')->with('error', 'Falha ao atualizar o comprovante.');
+        return redirect()->route('comprovante.index')->with('error', 'Falha ao atualizar o comprovante.');
     }
 
-    public function destroy(int $id): View
+    public function destroy(string $id): RedirectResponse
     {
-        return ($this->repository->delete($id))
-            ? view('comprovante.index')->with('sucess', 'Sucesso ao deletar comprovante.')
-            : view('comprovante.index')->with('error', 'Falha ao deletar comprovante.');
+        return ($this->repository->delete(intval($id)))
+            ? redirect()->route('comprovante.index')->with('success', 'Sucesso ao deletar comprovante.')
+            : redirect()->route('comprovante.index')->with('error', 'Falha ao deletar comprovante.');
     }
 
     private function find(int $id): object | null
