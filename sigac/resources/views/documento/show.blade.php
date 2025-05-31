226c4befd5a05b09baea917d9265b7843b79ca6e
QziuL <lf.quinholi2@gmail.com>
2025-05-31 20:57:43 -0300

    feat: Add logic to show a single document in view "show"

--- sigac/resources/views/documento/show.blade.php
+++ sigac/resources/views/documento/show.blade.php
@@ -1,3 +1,52 @@
-<div>
-    <!-- Smile, breathe, and go slowly. - Thich Nhat Hanh -->
-</div>
+@extends('layouts.main')
+
+@section('title', 'Ver documento')
+
+@section('content')
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+
+    <div class="container-fluid px-4 mt-4 ">
+        <div class="card">
+            <div class="card-body">
+                <h2>Solicitação nº {{ $documento->id }}</h2>
+                <div>
+                    <p>Descrição: <b>{{ $documento->descricao }}</b></p>
+                    <p>Categoria: <b>{{ $documento->categoria->nome }}</b></p>
+                    <p>Horas solicitadas: <b>{{ $documento->horas_in }} horas</b></p>
+                    @if(isset($documenot->comentario))
+                        <p>Comentário do admin: <b>{{ $documento->comentario }}</b></p>
+                    @endif
+                    <p>
+                        @if($documento->statuso == 0)
+                            Status: <b>PENDENTE</b>
+                        @else
+                            Status: <b>APROVADO</b>
+                       @endif
+                    </p>
+
+                    <button class="btn btn-primary mt-2">
+                        {{-- helper "url()" para acessar url base da aplicação (http://locahost:8000) --}}
+                        <a href="{{ url("storage/" . $documento->url) }}" class="text-decoration-none text-white">
+                            Visualizar documento
+                        </a>
+                    </button>
+                    <button class="btn btn-secondary mt-2">
+                        <a href="{{ route('documento.index') }} " class="text-decoration-none text-white">Voltar</a>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
