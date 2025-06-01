e87a36f525810255ec04bc35d19930a438c5e3f6
QziuL <lf.quinholi2@gmail.com>
2025-05-31 21:01:47 -0300

    refactor: Changed layout and styles

--- sigac/resources/views/documento/edit.blade.php
+++ sigac/resources/views/documento/edit.blade.php
@@ -3,40 +3,65 @@
 @section('title', 'Editar solicitação')
 
 @section('content')
-    <div>
-        <form action="{{ route('documento.update', $documento->id) }}" method="post" enctype="multipart/form-data">
-            @csrf
-            @method('put')
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
 
-            <div>
-                <label for="descricao">Descrição</label>
-                <input type="text" name="descricao" id="descricao" value="{{ $documento->descricao }}" required>
+    @if(session('success'))
+        <div class="alert alert-success">{{ session('success') }}</div>
+    @endif
+
+    <div class="container-fluid px-4 mt-4">
+        <div class="card">
+            <div class="card-body">
+                <h4>Editar Documento {{ $documento->id }}</h4>
+                <form action="{{ route('documento.update', $documento->id) }}"
+                      method="post"
+                      enctype="multipart/form-data">
+                    @csrf
+                    @method('put')
+
+                    <div class="mb-3">
+                        <label class="form-label" for="descricao">Descrição</label>
+                        <input class="form-control" type="text" name="descricao" id="descricao" value="{{ $documento->descricao }}" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label" for="horas_in">Horas solicitadas</label>
+                        <input class="form-control" type="number" name="horas_in" id="horas_in" value="{{ $documento->horas_in }}" required>
+                    </div>
+
+                    <div class="mb-3">
+                        <label class="form-label" for="categoria_id">Categoria</label>
+                        <select class="form-select mb-1"
+                                name="categoria_id" id="categoria_id" required>
+                            @foreach($categorias as $categoria)
+                                <option value="{{$categoria->id}}"
+                                    {{ ($documento->categoria->id == $categoria->id) ? 'selected' : '' }}>
+                                    {{ $categoria->nome }}
+                                </option>
+                            @endforeach
+                        </select>
+                    </div>
+                    <div>
+                        <label class="form-label" for="documento">Certificado</label>
+                        <input class="form-control" type="file" name="documento" id="documento" required>
+                    </div>
+                    <div>
+                        <button class="btn btn-primary mt-4" type="submit">Atualizar</button>
+                        <a class="btn btn-secondary mt-4"
+                           href="{{ route('documento.index') }}">
+                            Cancelar
+                        </a>
+                    </div>
+                </form>
             </div>
-            <div>
-                <label for="horas_in">Horas solicitadas</label>
-                <input type="number" name="horas_in" id="horas_in" value="{{ $documento->horas_in }}" required>
-            </div>
-            <div>
-                <x-input-label for="categoria_id" :value="__('Categoria')" />
-                <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        name="categoria_id" id="categoria_id" required>
-                    @foreach($categorias as $categoria)
-                        <option value="{{$categoria->id}}"
-                            {{ ($documento->categoria->id == $categoria->id) ? 'selected' : '' }}>
-                            {{ $categoria->nome }}
-                        </option>
-                    @endforeach
-                </select>
-            </div>
-            <div>
-                <label for="documento">Certificado</label>
-                <input type="file" name="documento" id="documento" required>
-            </div>
-            <div>
-                <x-primary-button class="ms-4">
-                    {{ __('Enviar') }}
-                </x-primary-button>
-            </div>
-        </form>
+        </div>
     </div>
 @endsection
